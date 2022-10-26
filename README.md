38f9da7f7f2b60639e2fc12791b72ade5f755343
marinelife <ghdic77@gmail.com>
2022-10-26 23:33:43 +0900

    image to ascii finished

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pythonVideoToASCII
 convert video frame to ascii from youtube videos
+
+## environment
+
+you need to install opencv
+
+* MacOS Ventura 13.0
+* python 3.11.0
+* pillow 9.2.0
+* opencv-python 4.6.0.66
+* pytube 12.1.0
---
+++ image_to_ascii.py
@@ -0,0 +1,40 @@
+import PIL.Image
+import json
+
+with open('settings.json') as f:
+    data = json.load(f)
+    ASCCI_CHARS = data['ASCII_CHARS']  # 그릴때 사용한 아스키코드
+    OUTPUT_WIDTH = data['OUTPUT_WIDTH']  # 출력 높이
+    DETAIL_PRIORITY = data['DETAIL_PRIORITY']  # 낮을수록 디테일하게 출력
+
+
+def resize_image(image, new_width):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio * 0.6)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return grayscale_image
+
+
+def pixels_to_ascii(image, detail_priority):
+    pixels = image.getdata()
+
+    characters = "".join([ASCCI_CHARS[(pixel // detail_priority) % len(ASCCI_CHARS)] for pixel in pixels])
+    return characters
+
+
+def image_to_ascii(image):
+    new_img_data = pixels_to_ascii(grayify(resize_image(image, OUTPUT_WIDTH)), DETAIL_PRIORITY)
+    pixel_cnt = len(new_img_data)
+    ascii_image = "\n".join(new_img_data[i:(i+OUTPUT_WIDTH)] for i in range(0, pixel_cnt, OUTPUT_WIDTH))
+    return ascii_image
+
+
+if __name__ == '__main__':
+    img = PIL.Image.open('bonsim.jpg')
+    print(image_to_ascii(img))
